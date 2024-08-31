c1070c670df7e445fe4c0be5f956b3e5e185c856
Kk <iankk0806@gmail.com>
2024-08-31 17:45:06 +0800

    feat: add notifications' models

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Notification(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=225, default="新訊息")
     message = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Notification
+
 # Create your views here.
+
+
+# 所有的通知，並按照時間的新舊順序排列
+def notifications_list(request):
+    notifications = Notification.objects.filter(user=request.ueser).order_by(
+        "-date_set"
+    )
