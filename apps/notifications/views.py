1923c60468342df5ef8c0f28337e240684b5e204
Kk <iankk0806@gmail.com>
2024-08-31 23:29:54 +0800

    feat: add notifications' views

--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -5,8 +5,13 @@ from .models import Notification
 # Create your views here.
 
 
-# 所有的通知，並按照時間的新舊順序排列
+
 def notifications_list(request):
-    notifications = Notification.objects.filter(user=request.ueser).order_by(
+    # 所有的通知，並按照時間的新舊順序排列
+    notifications = Notification.objects.filter(user=request.user).order_by(
         "-date_set"
     )
+    #未讀訊息的總數
+    unread_count=Notification.objects.filter(user=request.user,is_read=False).count()
+
+    return render(request,"notifications.html",{notifications:notifications,unread_count:unread_count})
